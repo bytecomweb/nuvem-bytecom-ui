495129f3d44ebcd9a32b9ff0909d57d50abb8442
Tomás <tomas153lm@gmail.com>
2026-03-30 16:13:54 -0300

    feat: adiciona renderização da instância padrão

--- src/components/telas/tela-whatsapp/tela-whatsapp.vue
+++ src/components/telas/tela-whatsapp/tela-whatsapp.vue
@@ -1,6 +1,18 @@
 <template>
   <TelaWhatsappCabecalho v-model:empresa-selecionada="empresaSelecionada" />
   <main class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 px-3 mt-2 gap-2">
+    <TelaWhatsappCardInstanciaPadrao
+      v-if="ehAdmin && instanciaPadrao?.instancia"
+      :instancia="instanciaPadrao.instancia"
+      :conexao="instanciaPadrao.conexao"
+      :monitoramento="instanciaPadrao.monitoramento"
+      :bloqueado="instanciaPadrao.configuracao.bloquearInstanciaPadraoWhatsApp"
+    />
+    <!-- @atualizar="refreshInstanciaPadrao"
+      @conectar="modalInstanciaPadraoConectarVisivel = true"
+      @desconectar="confirmarDesconexaoInstanciaPadrao"
+      @iniciar-monitoramento="tentaIniciarMonitoramentoInstanciaPadrao"
+      @parar-monitoramento="tentaPararMonitoramentoInstanciaPadrao" -->
     <TelaWhatsappCardInstancia
       v-for="(instancia, idx) in instanciasTratadas"
       :instancia
@@ -15,16 +27,20 @@
 </template>
 <script lang="ts" setup>
   import TelaWhatsappCabecalho from '@/components/telas/tela-whatsapp/components/tela-whatsapp-cabecalho.vue';
+  import TelaWhatsappCardInstanciaPadrao from '@/components/telas/tela-whatsapp/components/tela-whatsapp-card-instancia-padrao.vue';
   import TelaWhatsappCardInstancia from '@/components/telas/tela-whatsapp/components/tela-whatsapp-card-instancia.vue';
   import useApi from '@/composables/use-api';
   import useNotification from '@/composables/use-notification';
+  import obterWhatsAppInstanciaPadrao, {
+    DadosInstanciaPadrao,
+  } from '@/data/whatsapp/obter-whatsapp-instancia-padrao';
   import obterWhatsAppInstancias from '@/data/whatsapp/obter-whatsapp-instancias';
   import { Empresa } from '@/types/modelos/empresa';
   import { WhatsAppInstancia } from '@/types/modelos/whatsapp-instancia';
   import obterErroDaRequisicao from '@/utils/requisicao/obter-erro-da-requisicao';
   import { computed, onMounted, ref, watch } from 'vue';
 
-  const { bearerToken } = defineProps<{
+  const { bearerToken, ehAdmin } = defineProps<{
     bearerToken: string;
     ehAdmin?: boolean;
   }>();
@@ -36,6 +52,10 @@
   const instancias = ref<WhatsAppInstancia[]>([]);
 
   const instanciasTratadas = computed(() => {
+    if (!ehAdmin) {
+      return instancias.value;
+    }
+
     return instancias.value.filter((instancia) => !instancia.ehInstanciaPadrao);
   });
 
@@ -62,10 +82,31 @@
     }
   };
 
-  onMounted(tentaObterInstancias);
+  const instanciaPadrao = ref<DadosInstanciaPadrao>();
+
+  const tentaObterInstanciaPadrao = async () => {
+    try {
+      if (!empresaSelecionada.value) return;
+
+      const { dados } = await obterWhatsAppInstanciaPadrao(api, empresaSelecionada.value.id);
+
+      instanciaPadrao.value = dados;
+    } catch (err) {
+      erro(
+        obterErroDaRequisicao(err) ||
+          'Não foi possível obter a instância padrão de WhatsApp. Tente novamente mais tarde.'
+      );
+    }
+  };
+
+  onMounted(() => {
+    tentaObterInstancias();
+    tentaObterInstanciaPadrao();
+  });
 
   watch(empresaSelecionada, () => {
     instancias.value = [];
     tentaObterInstancias();
+    tentaObterInstanciaPadrao();
   });
 </script>
